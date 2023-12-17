8a7b1a95b92405abfbda9e79e3ed607e4f24aea3
annelrashka <annelrashkaperdana@gmail.com>
2023-12-17 18:18:37 +0700

    first commit

---
+++ services/line_notify_api.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+
+LINE_CHANNEL_ACCESS_TOKEN = '2002242568'
+LINE_CHANNEL_SECRET = '3f70f2eb5bd901214b44f77402366ceb'
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    # Get the request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+    reply_message = f"You said: {user_message}"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_message)
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
